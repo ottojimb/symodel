dd666e7c95d368477fc0f0afd494b147656dae27
Otto Jimenez <ottojimenez@gmail.com>
2020-06-20 18:43:55 -0500

    Relations added. Simple refactor

--- main.py
+++ main.py
@@ -12,44 +12,57 @@ sql_conn = psycopg2.connect(os.getenv("DATABASE_DSN"))
 ps_cursor = sql_conn.cursor(cursor_factory=RealDictCursor)
 
 
-def get_field(obj):
-    if obj["data_type"] == "integer":
-        line = f"{obj['column_name']} = fields.IntField("
-    elif obj["data_type"] == "numeric":
-        line = f"{obj['column_name']} = fields.DecimalField(max_digits={obj['numeric_precision']}, decimal_places={obj['numeric_scale']}, "
-    elif obj["data_type"] == "timestamp with time zone":
-        line = f"{obj['column_name']} = fields.DateTimeField("
-    elif obj["data_type"] == "date":
-        line = f"{obj['column_name']} = fields.DateField("
-    elif obj["data_type"] == "text":
-        line = f"{obj['column_name']} = fields.TextField("
-    elif obj["data_type"] == "character varying":
-        line = f"{obj['column_name']} = fields.CharField(max_length={obj['character_maximum_length']}, "
+def get_relationship(table_row, foreign_keys):
+    field = foreign_keys[table_row["column_name"]]
+    field_name = str(field["column_name"]).replace("_id", "")
+    class_name = "".join(word.title() for word in str(field["table_name"]).split("_"))
+    return f'{field_name} = fields.ForeignKeyField("models.{class_name}", related_name="{field["table_name"]}")'
+
+
+def get_data_type(table_row):
+    if table_row["data_type"] == "integer":
+        render_line = f"{table_row['column_name']} = fields.IntField("
+    elif table_row["data_type"] == "numeric":
+        render_line = f"{table_row['column_name']} = fields.DecimalField(max_digits={table_row['numeric_precision']}, decimal_places={table_row['numeric_scale']}, "
+    elif table_row["data_type"] == "timestamp with time zone":
+        render_line = f"{table_row['column_name']} = fields.DateTimeField("
+    elif table_row["data_type"] == "date":
+        render_line = f"{table_row['column_name']} = fields.DateField("
+    elif table_row["data_type"] == "text":
+        render_line = f"{table_row['column_name']} = fields.TextField("
+    elif table_row["data_type"] == "character varying":
+        render_line = f"{table_row['column_name']} = fields.CharField(max_length={table_row['character_maximum_length']}, "
     else:
-        return f"{obj['column_name']} = # VALIDATE_BY_HAND"
+        return f"{table_row['column_name']} = # VALIDATE_BY_HAND"
 
-    if obj["is_nullable"]:
-        line = f"{line}null=True"
+    if table_row["is_nullable"]:
+        render_line = f"{render_line}null=True"
     else:
-        line = f"{line}null=False"
+        render_line = f"{render_line}null=False"
 
-    line = f"{line})"
+    return f"{render_line})"
 
-    if str(obj["column_name"]).endswith("_id"):
+
+def get_field(table_row, foreign_keys):
+    if table_row["column_name"] in foreign_keys.keys():
+        return get_relationship(table_row, foreign_keys)
+
+    line = get_data_type(table_row)
+
+    if str(table_row["column_name"]).endswith("_id"):
         line = f"{line} # CHECK_RELATIONSHIP"
 
-    line = f"{line}"
     return line
 
 
-def inspect_table(table_name, obj):
+def inspect_table(table_name, table_description, foreign_keys):
     Jinja2.set_template("tortoise.py.jinja2")
 
     class_name = "".join(word.title() for word in table_name.split("_"))
     fields = []
 
-    for row in obj:
-        fields.append(get_field(row))
+    for table_row in table_description:
+        fields.append(get_field(table_row, foreign_keys))
 
     with open(f"output/{table_name}.py", "w") as f:
         f.write(
@@ -57,7 +70,7 @@ def inspect_table(table_name, obj):
         )
 
 
-def main():
+def get_fields():
     catalog = os.getenv("DB_CATALOG")
     schema = os.getenv("DB_SCHEMA")
     ps_cursor.execute(
@@ -71,13 +84,46 @@ def main():
     """
     )
     rows = ps_cursor.fetchall()
-    grouped = collections.defaultdict(list)
+    grouped_fields = collections.defaultdict(list)
 
     for row in rows:
-        grouped[row["table_name"]].append(row)
+        grouped_fields[row["table_name"]].append(row)
 
-    for key, values in grouped.items():
-        inspect_table(key, values)
+    return grouped_fields
+
+
+def get_foreign_keys(key):
+    ps_cursor.execute(
+        f"""
+            SELECT
+                tc.table_name, kcu.column_name,
+                ccu.table_name AS foreign_table_name,
+                ccu.column_name AS foreign_column_name
+            FROM
+                information_schema.table_constraints AS tc
+                JOIN information_schema.key_column_usage 
+                    AS kcu ON tc.constraint_name = kcu.constraint_name
+                JOIN information_schema.constraint_column_usage 
+                    AS ccu ON ccu.constraint_name = tc.constraint_name
+            WHERE constraint_type = 'FOREIGN KEY'
+            AND tc.table_name = '{key}'
+            ORDER BY tc.table_name, kcu.column_name;
+        """
+    )
+    rows = ps_cursor.fetchall()
+    grouped_foreign = dict()
+
+    for row in rows:
+        grouped_foreign[row["column_name"]] = row
+
+    return grouped_foreign
+
+
+def main():
+    grouped_fields = get_fields()
+
+    for table_name, table_configuration in grouped_fields.items():
+        inspect_table(table_name, table_configuration, get_foreign_keys(table_name))
 
 
 if __name__ == "__main__":
